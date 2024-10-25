82146e435e3e9a446116f553bc7e228c49dbe96c
Jakub Buler <buler.jakub@gmail.com>
2024-10-25 14:35:14 +0200

    changed to real data (brain WMH segmentation)

--- ig.py
+++ ig.py
@@ -4,184 +4,118 @@ import sys
 import torch
 import torch.nn as nn
 import numpy as np
-import matplotlib.pyplot as plt
+import nibabel as nib
 sys.path.append('/home/jr_buler/unet3d-xai/pytorch-3dunet/')
 from pytorch3dunet.unet3d.model import UNet3D
+from utils import *
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
 np.random.seed(42)
+import logging
+# logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
 
 '''
 This file creates a simple 3D U-Net model and demonstrates how to use Captum to compute and visualize
-integrated gradients for the model's output. Maxpool layers in the model are updated to have a kernel size of 1x3x3,
+integrated gradients. Maxpool layers in the model are updated to have a kernel size of 1x3x3,
 to imitate 2D pooling (to retain slice dimension in the intermediate feature maps).
-The model is trained on synthetic data with high intensity regions.
-
+The model is pretrained WMH Challenge data.
 '''
 
+data_path = '/media/dysk_a/jr_buler/WMH/dataverse_files/test/'
+data_instance = 'Amsterdam/Philips/160/'
+mri_file_path = data_path + data_instance + 'pre/FLAIR.nii.gz'
+mask_file_path = data_path + data_instance + 'wmh.nii.gz'
 
+model_path = './final_best_checkpoint.pytorch'
 
 # update MaxPool3d layers
-def update_maxpool3d_params(model, new_kernel_size, new_stride, new_padding):
-    for name, module in model.named_children():
-        if isinstance(module, nn.MaxPool3d):
-            setattr(model, name, nn.MaxPool3d(new_kernel_size, new_stride, new_padding))
-        elif len(list(module.children())) > 0:  # Recursively apply to nested modules
-            update_maxpool3d_params(module, new_kernel_size, new_stride, new_padding)
 
-
-# init the model
 net = UNet3D(in_channels=1, out_channels=1, f_maps=32, final_sigmoid=True, layer_order='gcr',
              num_groups=8)
-# New parameters for MaxPool3d layers
 new_kernel_size, new_stride, new_padding = (1, 3, 3), (1, 2, 2), (0, 1, 1)
-
 update_maxpool3d_params(net, new_kernel_size, new_stride, new_padding)
+
+model_state_dict = torch.load(model_path,
+                              map_location='cpu',
+                              weights_only=True)['model_state_dict']
+net.load_state_dict(model_state_dict)
 net.eval()
 
+# Load brain  data
+brain = {"image": np.asarray(nib.load(mri_file_path).dataobj),
+         "mask" : np.asarray(nib.load(mask_file_path).dataobj)}
+brain['image'] = torch.from_numpy(brain['image']).permute(2, 0, 1)
+brain['mask'] = torch.from_numpy(brain['mask']).permute(2, 0, 1)
 
-# input_tensor = torch.randn((1, 1, 20, 120, 120))
-# input_tensor = torch.ones((1, 1, 20, 120, 120))
-input_tensor = torch.zeros((1, 1, 20, 120, 120))
-input_tensor[:,:,10,:,:] = torch.ones(input_tensor[:,:,10,:,:].shape)
-input_tensor[:,:,15,50:70,:] = torch.zeros_like(input_tensor[:,:,15,50:70,:]) + 1
-input_tensor[:,:,5,:,50:70] = torch.zeros_like(input_tensor[:,:,5,:,50:70]) + 1
-slice_index = 17
-thickness = 5
-for h in range(input_tensor.shape[3]):  # Loop over height
-    for w in range(input_tensor.shape[4]):  # Loop over width
-        # Condition for a diagonal band (difference between height and width falls in range)
-        if abs(h - w) < thickness:
-            input_tensor[:, :, slice_index, h, w] = 1
+brain['mask'][brain['mask']==2] =  0# only WMH
 
-print(input_tensor.shape)
-
+#preprocessing - normalize image
+# brain['image'] = brain['image'] / brain['image'].max()
+# %%
+# get patches from the brain data
+x = 60
+brain_patch      = brain['image'][40:60, x:x+120, x:x+120].unsqueeze(0).unsqueeze(0)
+brain_mask_patch =  brain['mask'][40:60, x:x+120, x:x+120].unsqueeze(0).unsqueeze(0)
+brain_patch = brain_patch / brain_patch.max()
+# %%
+del brain
+plot_slices(brain_patch, "Patch slices")
+plot_slices(brain_mask_patch, "Ground truth")
+# %%
+# logger.info(f"input_tensor shape: {input_tensor.shape}")
+# input_tensor.requires_grad = True
+logger.info(f"input_tensor shape: {brain_patch.shape}")
+brain_patch.requires_grad = True
 #%%
-class Modified3DUNet(nn.Module):
+class ModelWrapper(nn.Module):
     def __init__(self, original_model):
-        super(Modified3DUNet, self).__init__()
+        super(ModelWrapper, self).__init__()
         self.original_model = original_model
+        logger.info(f"Wrapped model: {self.original_model}")
 
     def forward(self, x):
         # Run the original forward pass
         model_out = self.original_model(x)
-        print(f"model_out shape: {model_out.shape}")
-        out_max = (model_out > 0.9).type(torch.LongTensor)  # Shape: batch x channel x slice x height x width >>> channel to tutaj na outpucie są klasy za sigmoidą (final_sigmoid = True (default))
+        return model_out.sum(dim=(0, 2, 3, 4))  # [batch, num_classes]
+        logger.info(f"model_out shape: {model_out.shape}")
+        out_max = (model_out > 0.6).type(torch.LongTensor)  # Shape: batch x channel x slice x height x width >>> channel to tutaj na outpucie są klasy za sigmoidą (final_sigmoid = True (default))
+        if device.type == 'cuda':
+            out_max = out_max.cuda()
         selected_inds = torch.zeros_like(model_out).squeeze(1)  # Remove channel dimension (for scatter_)
-        print(f"selected_inds shape: {selected_inds.shape}")
-        print(f"out_max shape: {out_max.shape}")
+        logger.info(f"selected_inds shape: {selected_inds.shape}")
+        logger.info(f"model_out shape: {model_out.shape}")
         # Create a binary mask with 1 at predicted class locations
         selected_inds.scatter_(1, out_max.squeeze(1), 1)  # Scatter in dimension 1, using valid indices
-        print(f"selected_inds shape: {selected_inds.shape}")
+        logger.info(f"selected_inds shape: {selected_inds.shape}")
         # Element-wise multiplication of model output and the binary mask
         # and sum over spatial dimensions (slice, height, width)
-        print(f"model_out shape: {model_out.shape}")
-        print(f"selected_inds shape: {selected_inds.shape}")
-        print(f"{(model_out * selected_inds).sum(dim=(2,3,4))=}")
-        # return (model_out * selected_inds).sum(dim=(2, 3, 4))  # [batch, num_classes]
+        logger.info(f"{(model_out * selected_inds).sum(dim=(0, 2, 3, 4)).shape=}")
         return (model_out * selected_inds).sum(dim=(0, 2, 3, 4))  # [batch, num_classes]
-unet = Modified3DUNet(net)
 
-
-# %%
-# Generate synthetic training data
-def generate_synthetic_data(num_samples, shape, high_intensity_value=1, low_intensity_value=0):
-    data = []
-    targets = []
-    for _ in range(num_samples):
-        sample = torch.zeros(shape)
-        target = torch.zeros(shape)
-        
-        # Randomly place high intensity regions
-        num_high_intensity_regions = np.random.randint(1, 5)
-        for _ in range(num_high_intensity_regions):
-            z = np.random.randint(0, shape[1])
-            y = np.random.randint(0, shape[2])
-            x = np.random.randint(0, shape[3])
-            depth = np.random.randint(1, shape[1] // 4)
-            height = np.random.randint(1, shape[2] // 4)
-            width = np.random.randint(1, shape[3] // 4)
-            
-            sample[:, z:z+depth, y:y+height, x:x+width] = high_intensity_value
-            target[:, z:z+depth, y:y+height, x:x+width] = 1
-        
-        data.append(sample)
-        targets.append(target)
-    
-    return torch.stack(data), torch.stack(targets)
-
-# Generate 10 samples of shape (1, 20, 120, 120)
-num_samples = 10
-shape = (1, 20, 120, 120)
-data, targets = generate_synthetic_data(num_samples, shape)
-
-# Define a simple training loop
-def train_unet(model, data, targets, num_epochs=5, learning_rate=0.001):
-    criterion = nn.BCELoss()  # Binary Cross Entropy Loss
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    
-    for epoch in range(num_epochs):
-        model.train()
-        optimizer.zero_grad()
-        
-        outputs = model(data)
-        outputs = outputs - min(outputs.flatten())
-        outputs = outputs / outputs.max()  # Normalize to [0, 1]
-        loss = criterion(outputs, targets)
-        
-        loss.backward()
-        optimizer.step()
-        
-        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}")
-
-# Train the model
-if 0:
-    train_unet(unet.original_model, data, targets)
-
-
+unet = ModelWrapper(net)
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") # gpu 24gb is not enough for such big patch
+device = torch.device("cpu")
+logger.info(f"Device set to: {device}")
+unet.to(device)
+brain_patch = brain_patch.to(device)
+prediction = unet.original_model(brain_patch).detach()
+prediction = (prediction > 0.6).type(torch.LongTensor)
+# threshold prediction
+plot_slices(prediction, "Prediction", cmap='gray')
 # %%
 ig = IntegratedGradients(unet, multiply_by_inputs=False)
-input_tensor.requires_grad = True
+brain_patch.requires_grad = True
 unet.eval()
-attribution = ig.attribute(input_tensor, n_steps=50)
-
-nrows, ncols = 5, 4
-fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(8, 10))
-
-max_attr = attribution.flatten().max()
-min_attr = attribution.flatten().min()
-
-for i in range(attribution.shape[2]):
-    # Select the i-th slice along the depth axis
-    slice_attr = attribution[0, 0, i, :, :].cpu().detach().numpy()
-    slice_attr = np.expand_dims(slice_attr, axis=-1)
-    print(f"slice {i}", end=':\t')
-    print(min(slice_attr.flatten()), end='\t')
-    print(max(slice_attr.flatten()), end='\t')
-    print(slice_attr.flatten().mean())
-
-    row = i // ncols
-    col = i % ncols
-    ax = axes[row, col]
-    axes[row, col].imshow(slice_attr, cmap='coolwarm')#, vmin=min_attr, vmax=max_attr)
-    axes[row, col].axis('off')
-    axes[row, col].set_title(f'slice no. {i}')
-plt.tight_layout()
-plt.show()
-
+attribution = ig.attribute(brain_patch, n_steps=50)
 # %%
-nrows, ncols = 5, 4
-fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(8, 10))
-for i in range(input_tensor.shape[2]):
-    # Select the i-th slice along the depth axis
-    slice_attr = input_tensor[0, 0, i, :, :].cpu().detach().numpy()
-    slice_attr = np.expand_dims(slice_attr, axis=-1)
-    row = i // ncols
-    col = i % ncols
-    ax = axes[row, col]
-    axes[row, col].imshow(slice_attr, cmap='coolwarm', vmin=0, vmax=1)
-    axes[row, col].axis('off')
-    axes[row, col].set_title(f'slice no. {i}')
-plt.tight_layout()
-plt.show()
+absolute_attributions = torch.abs(attribution)
+flattened_attributions = absolute_attributions.view(-1).cpu().detach().numpy()
+percentile_99 = np.percentile(flattened_attributions, 99)
+capped_attributions = torch.clamp(absolute_attributions, max=percentile_99)
+min_val = capped_attributions.min()
+max_val = capped_attributions.max()
+normalized_attributions = (capped_attributions - min_val) / (max_val - min_val + 1e-10)
+plot_slices(normalized_attributions, "Integrated Gradients", cmap='gray')
 # %%
--- layer_ig.py
+++ layer_ig.py
@@ -4,9 +4,10 @@ import sys
 import torch
 import torch.nn as nn
 import numpy as np
-import matplotlib.pyplot as plt
+import nibabel as nib
 sys.path.append('/home/jr_buler/unet3d-xai/pytorch-3dunet/')
 from pytorch3dunet.unet3d.model import UNet3D
+from utils import *
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
 np.random.seed(42)
@@ -17,60 +18,66 @@ logger = logging.getLogger(__name__)
 
 '''
 This file creates a simple 3D U-Net model and demonstrates how to use Captum to compute and visualize
-integrated gradients for specified layer. Maxpool layers in the model are updated to have a kernel size of 1x3x3,
+integrated gradients. Maxpool layers in the model are updated to have a kernel size of 1x3x3,
 to imitate 2D pooling (to retain slice dimension in the intermediate feature maps).
-The model is trained on synthetic data with high intensity regions.
+The model is pretrained WMH Challenge data.
 
 Inplace call of activation functions was set to False (default was True) in the buildingblocks.py file
 in the pytorch3dunet package.
 '''
 
+data_path = '/media/dysk_a/jr_buler/WMH/dataverse_files/test/'
+data_instance = 'Amsterdam/Philips/160/'
+mri_file_path = data_path + data_instance + 'pre/FLAIR.nii.gz'
+mask_file_path = data_path + data_instance + 'wmh.nii.gz'
 
-# update MaxPool3d layers
-def update_maxpool3d_params(model, new_kernel_size, new_stride, new_padding):
-    for name, module in model.named_children():
-        if isinstance(module, nn.MaxPool3d):
-            setattr(model, name, nn.MaxPool3d(new_kernel_size, new_stride, new_padding))
-        elif len(list(module.children())) > 0:  # Recursively apply to nested modules
-            update_maxpool3d_params(module, new_kernel_size, new_stride, new_padding)
+model_path = './final_best_checkpoint.pytorch'
 
-
-# init the model
+# load model and update MaxPool3d layers
 net = UNet3D(in_channels=1, out_channels=1, f_maps=32, final_sigmoid=True, layer_order='gcr',
              num_groups=8)
-# New parameters for MaxPool3d layers
 new_kernel_size, new_stride, new_padding = (1, 3, 3), (1, 2, 2), (0, 1, 1)
-
 update_maxpool3d_params(net, new_kernel_size, new_stride, new_padding)
+model_state_dict = torch.load(model_path,
+                              map_location='cpu',
+                              weights_only=True)['model_state_dict']
+net.load_state_dict(model_state_dict)
 net.eval()
-# input_tensor = torch.randn((1, 1, 20, 120, 120))
-# input_tensor = torch.ones((1, 1, 20, 120, 120))
-input_tensor = torch.zeros((1, 1, 20, 120, 120))
-input_tensor[:,:,10,:,:] = torch.ones(input_tensor[:,:,10,:,:].shape)
-input_tensor[:,:,15,50:70,:] = torch.zeros_like(input_tensor[:,:,15,50:70,:]) + 1
-input_tensor[:,:,5,:,50:70] = torch.zeros_like(input_tensor[:,:,5,:,50:70]) + 1
-slice_index = 17
-thickness = 5
-for h in range(input_tensor.shape[3]):  # Loop over height
-    for w in range(input_tensor.shape[4]):  # Loop over width
-        # Condition for a diagonal band (difference between height and width falls in range)
-        if abs(h - w) < thickness:
-            input_tensor[:, :, slice_index, h, w] = 1
 
-logger.info(f"input_tensor shape: {input_tensor.shape}")
-input_tensor.requires_grad = True
+# load brain  data
+brain = {"image": np.asarray(nib.load(mri_file_path).dataobj),
+         "mask" : np.asarray(nib.load(mask_file_path).dataobj)}
+brain['image'] = torch.from_numpy(brain['image']).permute(2, 0, 1)
+brain['mask'] = torch.from_numpy(brain['mask']).permute(2, 0, 1)
+
+brain['mask'][brain['mask']==2] =  0# only WMH
+
+#preprocessing - normalize image
+brain['image'] = brain['image'] / brain['image'].max()
+
+# get patches from the brain data: 20 x 120 x 120
+x = 60
+brain_patch      = brain['image'][40:60, x:x+120, x:x+120].unsqueeze(0).unsqueeze(0)
+brain_mask_patch =  brain['mask'][40:60, x:x+120, x:x+120].unsqueeze(0).unsqueeze(0)
+del brain
+# show brain patch and mask
+plot_slices(brain_patch, "Patch slices")
+plot_slices(brain_mask_patch, "Ground truth")
+logger.info(f"input_tensor shape: {brain_patch.shape}")
+brain_patch.requires_grad = True
 #%%
-class Modified3DUNet(nn.Module):
+class ModelWrapper(nn.Module):
     def __init__(self, original_model):
-        super(Modified3DUNet, self).__init__()
+        super(ModelWrapper, self).__init__()
         self.original_model = original_model
         logger.info(f"Wrapped model: {self.original_model}")
 
     def forward(self, x):
         # Run the original forward pass
         model_out = self.original_model(x)
+        return model_out.sum(dim=(2, 3, 4))  # [batch, num_classes]
         logger.info(f"model_out shape: {model_out.shape}")
-        out_max = (model_out > 0.9).type(torch.LongTensor)  # Shape: batch x channel x slice x height x width >>> channel to tutaj na outpucie są klasy za sigmoidą (final_sigmoid = True (default))
+        out_max = (model_out > 0.6).type(torch.LongTensor)  # Shape: batch x channel x slice x height x width >>> channel to tutaj na outpucie są klasy za sigmoidą (final_sigmoid = True (default))
         if device.type == 'cuda':
             out_max = out_max.cuda()
         selected_inds = torch.zeros_like(model_out).squeeze(1)  # Remove channel dimension (for scatter_)
@@ -81,114 +88,34 @@ class Modified3DUNet(nn.Module):
         logger.info(f"selected_inds shape: {selected_inds.shape}")
         # Element-wise multiplication of model output and the binary mask
         # and sum over spatial dimensions (slice, height, width)
-        logger.info(f"{(model_out * selected_inds).sum(dim=(2,3,4))=}")
         logger.info(f"{(model_out * selected_inds).sum(dim=(2, 3, 4)).shape=}")
         return (model_out * selected_inds).sum(dim=(2, 3, 4))  # [batch, num_classes]
 
-unet = Modified3DUNet(net)
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") # gpu 24gb is not enough
+unet = ModelWrapper(net)
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") # gpu 24gb is not enough for such big patch
 device = torch.device("cpu")
 logger.info(f"Device set to: {device}")
 unet.to(device)
-input_tensor = input_tensor.to(device)
-# %%
-# Generate synthetic training data
-
-def generate_synthetic_data(num_samples, shape, high_intensity_value=1, low_intensity_value=0):
-    data = []
-    targets = []
-    for _ in range(num_samples):
-        sample = torch.zeros(shape)
-        target = torch.zeros(shape)
-        
-        # Randomly place high intensity regions
-        num_high_intensity_regions = np.random.randint(1, 5)
-        for _ in range(num_high_intensity_regions):
-            z = np.random.randint(0, shape[1])
-            y = np.random.randint(0, shape[2])
-            x = np.random.randint(0, shape[3])
-            depth = np.random.randint(1, shape[1] // 4)
-            height = np.random.randint(1, shape[2] // 4)
-            width = np.random.randint(1, shape[3] // 4)
-            
-            sample[:, z:z+depth, y:y+height, x:x+width] = high_intensity_value
-            target[:, z:z+depth, y:y+height, x:x+width] = 1
-        
-        data.append(sample)
-        targets.append(target)
-    
-    return torch.stack(data), torch.stack(targets)
-def train_unet(model, data, targets, num_epochs=5, learning_rate=0.001):
-    criterion = nn.BCELoss()  # Binary Cross Entropy Loss
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    
-    for epoch in range(num_epochs):
-        model.train()
-        optimizer.zero_grad()
-        
-        outputs = model(data)
-        outputs = outputs - min(outputs.flatten())
-        outputs = outputs / outputs.max()  # Normalize to [0, 1]
-        loss = criterion(outputs, targets)
-        
-        loss.backward()
-        optimizer.step()
-        
-        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}")
-if 0:
-# Generate 10 samples of shape (1, 20, 120, 120)
-    num_samples = 10
-    shape = (1, 20, 120, 120)
-    data, targets = generate_synthetic_data(num_samples, shape)
-    data = data.to(device)
-    targets = targets.to(device)
-    # Define a simple training loop
-    # Train the model
-    train_unet(unet.original_model, data, targets)
+brain_patch = brain_patch.to(device)
+prediction = unet.original_model(brain_patch).detach()
+prediction = (prediction > 0.6).type(torch.LongTensor)
+# threshold prediction - round ==> th=0.5
+plot_slices(prediction, "Prediction slices")
 # %%
 unet.eval()
 target_layers = [encoder.basic_module[-1].conv for encoder in unet.original_model.encoders]
-
-
-
 for i, target_layer in enumerate(target_layers):
-    layer_= LayerIntegratedGradients(unet, target_layer)
-    attribution = layer_.attribute(input_tensor, target=0)
-    # attribution = attribution.mean(dim=1, keepdim=True)
-    attribution, _ = attribution.max(dim=1, keepdim=True)
-
-    nrows, ncols = 5, 4
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(8, 10))
-
-    # max_attr = attribution.flatten().max()
-    # min_attr = attribution.flatten().min()
-
-    for j in range(attribution.shape[2]):
-        # Select the i-th slice along the depth axis
-        slice_attr = attribution[0, 0, j, :, :].cpu().detach().numpy()
-        slice_attr = np.expand_dims(slice_attr, axis=-1)
-        logger.info(f"slice {j}:\t{min(slice_attr.flatten())}\t{max(slice_attr.flatten())}\t{slice_attr.flatten().mean()}")
-        ROW, COL = j // ncols, j % ncols
-        ax = axes[ROW, COL]
-        axes[ROW, COL].imshow(slice_attr, cmap='coolwarm')#, vmin=min_attr, vmax=max_attr)
-        axes[ROW, COL].axis('off')
-        axes[ROW, COL].set_title(f'slice no. {j}')
-    fig.suptitle(f"Encoder no. {i} last conv layer")
-    plt.tight_layout()
-    plt.show()
-# %%
-nrows, ncols = 5, 4
-fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(8, 10))
-for i in range(input_tensor.shape[2]):
-    # Select the i-th slice along the depth axis
-    slice_attr = input_tensor[0, 0, i, :, :].cpu().detach().numpy()
-    slice_attr = np.expand_dims(slice_attr, axis=-1)
-    row = i // ncols
-    col = i % ncols
-    ax = axes[row, col]
-    axes[row, col].imshow(slice_attr, cmap='coolwarm', vmin=0, vmax=1)
-    axes[row, col].axis('off')
-    axes[row, col].set_title(f'slice no. {i}')
-plt.tight_layout()
-plt.show()
+    layer_= LayerIntegratedGradients(unet, target_layer, multiply_by_inputs=False)
+    attribution = layer_.attribute(brain_patch, target=0)
+    attribution_max, _ = attribution.max(dim=1, keepdim=True)
+    absolute_attributions = torch.abs(attribution_max)
+    flattened_attributions = absolute_attributions.view(-1).cpu().detach().numpy()
+    percentile_99 = np.percentile(flattened_attributions, 99)
+    capped_attributions = torch.clamp(absolute_attributions, max=percentile_99)
+    min_val = capped_attributions.min()
+    max_val = capped_attributions.max()
+    normalized_attributions = (capped_attributions - min_val) / (max_val - min_val + 1e-10)
+    plot_slices(normalized_attributions, f"layer no. {i} integrated gradients")
+    print(normalized_attributions.shape)
+print("Done")
 # %%
